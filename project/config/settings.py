6a4a9c2b3b46d0aff5f778ca41b6fbba810b6731
pshyeok <pshyeok@kakao.com>
2023-05-01 15:35:58 +0900

    bootstrap

--- project/config/settings.py
+++ project/config/settings.py
@@ -110,6 +110,8 @@ TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
@@ -117,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- project/pybo/views.py
+++ project/pybo/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 # from django.http import HttpResponse
 # Create your views here.
 from .models import Question
+from .forms import QuestionForm
+
+
+
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
@@ -12,3 +17,23 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id): 
+    question = get_object_or_404(Question, pk=question_id)
+    content = request.POST.get('content')
+    if content:
+        question.answer_set.create(content=content, create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
