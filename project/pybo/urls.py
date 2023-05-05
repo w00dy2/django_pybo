fcd55aa937320e4fb625f0b6555c60b1a233a984
pshyeok <pshyeok@kakao.com>
2023-05-06 02:14:16 +0900

    3-10 views.py_ver.01

--- project/pybo/urls.py
+++ project/pybo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from . import views
+from .views import base_views, question_views, answer_views
 
 app_name = 'pybo'
 
